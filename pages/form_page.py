8d52d81731ea4cfc09d4ed79dbce9bffc0f1e2fa
alexandrserch <alexandr.serch.qal@gmail.com>
2024-09-10 20:06:53 +0300

    First Commit

---
+++ pages/form_page.py
@@ -0,0 +1,8 @@
+from pages.base_page import BasePage
+from locators.form_page_locators import FormPageLocators as Locators
+
+
+class FormPage(BasePage):
+
+    def fill_fields_and_submit(self):
+        pass
